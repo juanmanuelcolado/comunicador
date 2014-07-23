254ce05c221a34cb954279f277c253459c4f39a0
Nadia Faya <nadiafaya@gmail.com>
2014-07-22 21:46:53 -0300

    Cleaned up code formating

--- www/js/home/controllers/homeCtrl.js
+++ www/js/home/controllers/homeCtrl.js
@@ -1,27 +1,27 @@
 communicatorApp.controller('homeCtrl', function($scope) {
-	$scope.phases = [
-		{ 
-			name: 'Fase 1',
-			selected: true
-		},{ 
-			name: 'Fase 2',
-			selected: false
-		},{ 
-			name: 'Fase 3',
-			selected: false
-		},{ 
-			name: 'Fase 4',
-			selected: false
-		},{ 
-			name: 'Fase 5',
-			selected: false
-		},{ 
-			name: 'Fase 6',
-			selected: false
-		}];
+    $scope.phases = [
+        { 
+            name: 'Fase 1',
+            selected: true
+        },{ 
+            name: 'Fase 2',
+            selected: false
+        },{ 
+            name: 'Fase 3',
+            selected: false
+        },{ 
+            name: 'Fase 4',
+            selected: false
+        },{ 
+            name: 'Fase 5',
+            selected: false
+        },{ 
+            name: 'Fase 6',
+            selected: false
+        }];
 
-	$scope.selectPhase = function(phase) {
-		$scope.phases.map(function(item) { item.selected = false; });
-		phase.selected = true;
-	};
+    $scope.selectPhase = function(phase) {
+        $scope.phases.map(function(item) { item.selected = false; });
+        phase.selected = true;
+    };
 });
