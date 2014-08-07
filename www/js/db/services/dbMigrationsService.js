a0b2977a6799b428446c3df115ea624d6888c5aa
Nicolás Santángelo <santangelonicolas@gmail.com>
2014-08-06 21:04:18 -0300

    Clase Migration para generar migraciones

--- www/js/db/services/dbMigrationsService.js
+++ www/js/db/services/dbMigrationsService.js
@@ -1,9 +1,32 @@
 communicatorApp.service('dbMigrationsService', function() {
+    var Migration = function(tableName) {
+        this.tableName = tableName;
+        this.query = '';
+        this.createTable();
+    };
+
+    Migration.prototype = {
+        createTable: function () {
+           this.query += 'CREATE TABLE IF NOT EXISTS ' + this.tableName + ';\n';
+           return this;
+        },
+        addColumn: function (column) {
+           this.query += 'ALTER TABLE ' + this.tableName + ' ADD COLUMN ' + column + ';\n';
+           return this;
+        },
+        run: function(operations) {
+            operations.call(this);
+            return this;
+        }
+    };
+
     return {
         migrations: [
-            { query: 'CREATE TABLE IF NOT EXISTS Card' },
-            { query: 'ALTER TABLE Card ADD COLUMN Id INTEGER PRIMARY KEY ASC' },
-            { query: 'ALTER TABLE Card ADD COLUMN Title TEXT' }
+            new Migration('Card')
+                .addColumn('Id INTEGER PRIMARY KEY ASC')
+                .addColumn('Title TEXT')
+
+            // Some other table
         ]
     };
 });
