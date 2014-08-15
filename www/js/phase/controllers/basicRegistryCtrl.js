de082a9fb16b68999af9a7e591e2360a74d504b7
Nadia Faya <nadiafaya@gmail.com>
2014-08-15 17:07:34 -0300

    Added double back button and dynamic user select in basic registry

--- www/js/phase/controllers/basicRegistryCtrl.js
+++ www/js/phase/controllers/basicRegistryCtrl.js
@@ -1,3 +1,40 @@
-communicatorApp.controller('basicRegistryCtrl', function($scope) {
+communicatorApp.controller('basicRegistryCtrl', function($scope, currentReceptorService) {
+	$scope.registry = {
 
+	};
+
+	$scope.users = [
+		{
+			name: "Juan",
+			lastName: "Perez",
+			advanced: false
+		},
+		{
+			name: "Pedro",
+			lastName: "Troglio",
+			advanced: true
+		}
+	];
+
+	// get users
+	// select current user?
+
+	$scope.changeUser = function() {
+		currentReceptorService.change($scope)
+	};
+
+	$scope.saveRegistry = function() {
+
+	};
+
+	$scope.goBack = function() {
+		// this is to force a double "back"
+	  	var backView = $scope.$viewHistory.views[$scope.$viewHistory.backView.backViewId];
+	    $scope.$viewHistory.forcedNav = {
+	        viewId:     backView.viewId,
+	        navAction: 'moveBack',
+	        navDirection: 'back'
+	    };
+	    backView.go();
+	};
 });
