54693a43b19dd338fcec2a5eb0476b84659bc236
NicoSantangelo <santangelonicolas@gmail.com>
2014-08-20 17:18:48 -0300

    Métodos para una configuración. Encontrar y setear registros. Pantalla de configuración

--- www/js/configuration/controllers/configurationsCtrl.js
+++ www/js/configuration/controllers/configurationsCtrl.js
@@ -1,2 +1,5 @@
-communicatorApp.controller('configurationsCtrl', function($scope, QueryBuilderService) {
+communicatorApp.controller('configurationsCtrl', function($scope, configurationDbService) {
+	// configurationDbService.set({ key:"IP", value:"127.0.0.1" }).then(function(results) {
+		
+	// });
 });
--- www/js/db/services/configurationDbService.js
+++ www/js/db/services/configurationDbService.js
@@ -1,8 +1,24 @@
 communicatorApp.service('configurationDbService', function(QueryBuilderService) {
-    return new QueryBuilderService("Configuration").define("find", function(key) {
+    var queryBuilder = new QueryBuilderService("Configuration");
+
+	queryBuilder.define("find", function(key) {
         return {
             query: 'SELECT * FROM ' + this.tableName + ' WHERE key = ?',
             args: [key]
         };
     });
+
+	queryBuilder.set = function(configuration) {
+    	var self = this;
+    	return this.find(configuration.key).then(function(results) {
+    		if (results.length) {
+    			configuration.id = results[0].id;
+    			return self.update(configuration);
+    		} else {
+    			return self.insert(configuration);
+    		}
+    	});
+    };
+	
+	return queryBuilder
 });
