f6fc08b619be123c35330ee7a741b4f31ba26c77
Nicolás Santángelo <santangelonicolas@gmail.com>
2014-08-26 00:23:44 -0300

    Current user service (stored in configuration)

--- www/js/configuration/services/configurationService.js
+++ www/js/configuration/services/configurationService.js
@@ -1,5 +1,13 @@
 communicatorApp.service('configurationService', function(configurationDbService, $q) {
     var db = configurationDbService;
+    var addToPartialResult = function(keyName) {
+        var returnValue = {};
+        return function(result) {
+            returnValue[keyName] = result;
+            return returnValue;
+        };
+    };
+
     return {
         get: function(key) {
             var deferred = $q.defer();
@@ -7,6 +15,25 @@ communicatorApp.service('configurationService', function(configurationDbService,
                 var configuration = results.length > 0 ? results[0] : { value: undefined };
                 deferred.resolve(configuration.value);
             });
+            return deferred.promise;
+        },
+        getMultiple: function(keys) {
+            var deferred = $q.defer();
+            var promises = [];
+
+            for(var keyName in keys) {
+                promises.push(
+                    this.get(keyName).then(addToPartialResult(keys[keyName]))
+                );
+            }
+
+            $q.all(promises).then(function(results) {
+                var mergedResult = results.reduce(function(memo, current) {
+                    return angular.extend(memo, current);
+                });
+                deferred.resolve(mergedResult);
+            });
+
             return deferred.promise;
         },
         set: function(key, value) {
@@ -20,6 +47,21 @@ communicatorApp.service('configurationService', function(configurationDbService,
                     return db.insert(configuration);
                 }
             });
+        },
+        setMultiple: function(configurations) {
+            var deferred = $q.defer();
+            var promises = [];           
+
+            for(var key in configurations) {
+                promises.push(
+                    this.set(key, configurations[key])
+                );
+            }
+ 
+            $q.all(promises).then(function(results) {
+                deferred.resolve(results);
+            });
+            return deferred.promise;
         }
     };
 });
