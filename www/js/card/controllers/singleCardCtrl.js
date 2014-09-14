9a293536177a827947e5c8c33b898b7275be60cc
Sanuzzi <occhiuzzi.santiago@gmail.com>
2014-09-14 08:57:58 -0300

    Fotos en pictogramas

--- www/js/card/controllers/singleCardCtrl.js
+++ www/js/card/controllers/singleCardCtrl.js
@@ -1,9 +1,10 @@
-communicatorApp.controller('singleCardCtrl', function($scope, $stateParams, $ionicNavBarDelegate, cardDbService) {
+communicatorApp.controller('singleCardCtrl', function($scope, $stateParams, $ionicNavBarDelegate, cardDbService, imageUploaderService) {
     $scope.creating = !$stateParams.id;
+    $scope.cameraIsEnabled = imageUploaderService.cameraIsEnabled;
     
     $scope.card = {
         title: '',
-        img: "ionic.png",
+        img: imageUploaderService.defaultSrc,
         enabled: true
     };
 
@@ -24,8 +25,15 @@ communicatorApp.controller('singleCardCtrl', function($scope, $stateParams, $ion
         } else {
             cardDbService.update($scope.card);
         }
-
         $scope.goBack();
     };
 
+    var updateCardImage = function(newImageSrc) {
+        $scope.card.img = newImageSrc;
+        $scope.$apply();
+    };
+
+    $scope.takePicture = function() {
+        imageUploaderService.takePicture(updateCardImage);
+    };
 });
