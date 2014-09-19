0dd561db60addbb62e15bd521c85409ca4525675
Nadia Faya <nadiafaya@gmail.com>
2014-09-18 23:47:46 -0300

    Se arregla el abrir y cerrar de los items en la home

--- www/js/home/controllers/homeCtrl.js
+++ www/js/home/controllers/homeCtrl.js
@@ -12,25 +12,15 @@ communicatorApp.controller('homeCtrl', function($scope, levelDbService) {
         $scope.selectedLevel = $scope.levels[lastLevel];
     });
 
-    $scope.selectLevel = function(levels, level) {
-        var levelEnabled = false;
-        for (var i = 0; i < levels.length; i++) {
-            levels[i].selected = false;
-        }
-        levelDbService.selectAll().then(function(results) {
-            for (var i = 0; i < results.length; i++) {
-                if (results[i].description == level.description && results[i].enabled == "true"){
-                    level.selected = true;
-                    levelEnabled = true;
-                    $scope.selectedLevel = level;
-                }
+    $scope.selectLevel = function(level) {
+        if ($scope.selectedLevel.id === level.id) {
+            level.selected = !level.selected;
+        } else {
+            if (level.enabled === "true") {
+                $scope.selectedLevel.selected = false;
+                level.selected = true;
+                $scope.selectedLevel = level;
             }
-        });
-        //if the selected level is disabeld then we select the first level
-        if(!levelEnabled){
-            levels[0].selected = true;
-            $scope.selectedLevel = levels[0];
-            //TODO: message "El nivel seleccionado no se encuentra disponible"
         }
     };
 });
