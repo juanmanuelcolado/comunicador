5400a4f509f2d5f92c1d21b350f762e1e2990e6c
Nicolás Santángelo <santangelonicolas@gmail.com>
2014-10-16 00:09:02 -0300

    Buscar receptores por el nombre completo

--- www/js/db/services/receiverDbService.js
+++ www/js/db/services/receiverDbService.js
@@ -2,7 +2,7 @@ communicatorApp.service('receiverDbService', function(QueryBuilderService) {
     return new QueryBuilderService('Receiver')
         .define("notInternal", function() {
             return {
-                query: 'SELECT * FROM ' + this.tableName + ' WHERE internal = ?',
+                query: "SELECT *, (name || ' ' || lastName) as fullName  FROM " + this.tableName + " WHERE internal = ?",
                 args: [0]
             };
         });
