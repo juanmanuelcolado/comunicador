a20fa90075d3020e50b675b09f6ca49e83fcf54f
Nicolás Santángelo <santangelonicolas@gmail.com>
2014-10-17 01:02:44 -0300

    Refactorizar para escribir un poco menos

--- www/js/tutorial/services/tutorialService.js
+++ www/js/tutorial/services/tutorialService.js
@@ -23,40 +23,26 @@ communicatorApp.service('tutorialService', function($state, $ionicPopup, $timeou
             switch($state.current.name) {
                 case 'tutorialHome':
                     this.step('Iniciar nivel.', 'Este tutorial te llevará a través de las funciones básicas de la aplicación.<br/><br/>Para comenzar una actividad se debe presionar IR', {
-                        back: function() {
-                            $state.transitionTo("app.home");
-                        },
-                        next: function() {
-                            $state.transitionTo("tutorialLevelCards", { levelNumber: 1 });
-                        }
+                        back: { state: "app.home" },
+                        next: { state: "tutorialLevelCards", params: { levelNumber: 1 } }
                     });
                     closeEvent.attach();
                     break;
                 case 'tutorialLevelCards':
                     this.step('Seleccionar pictograma', 'Al comenzar un intercambio se debe seleccionar un pictograma de la lista.<br/>En esta sección solo se mostraran los pictogramas habilitados', {
-                        back: function() {
-                            $state.transitionTo("tutorialHome");
-                        },
-                        next: function() {
-                            $state.transitionTo("tutorialLevelSingleCard", { id: 1 });
-                        }
+                        back: { state: "tutorialHome" },
+                        next: { state: "tutorialLevelSingleCard", params: { id: 1 } }
                     });
                     break;
                 case 'tutorialLevelSingleCard':
                     this.step('Intercambio', 'Se muestra el pictograma para que pueda ser visto claramente y entregado al receptor por el usuario.<br /><br />Para puntuar el intercambio, puede presionarce el botón de menú o mantener precionado sobre la imagen', {
-                        back: function() {
-                            $state.transitionTo("tutorialLevelCards", { levelNumber: 1 });
-                        },
-                        next: function() {
-                            $state.transitionTo("tutorialPatternLock");
-                        }
+                        back: { state: "tutorialLevelCards", params: { levelNumber: 1 } },
+                        next: { state: "tutorialPatternLock" }
                     });
                     break;
                 case 'tutorialPatternLock':
                     this.step('Registrar intercambio', 'Al registrar un intercabio se deben seleccionar los comportamientos que ocurrieron durante el mismo.', {
-                        back: function() {
-                            $state.transitionTo("tutorialLevelSingleCard", { id: 1 });
-                        },
+                        back: { state: "tutorialLevelSingleCard", params: { id: 1 } },
                         next: function() {
                             $state.transitionTo("app.home").then(function() {
                                 var lastPopup = $ionicPopup.alert({
@@ -74,17 +60,26 @@ communicatorApp.service('tutorialService', function($state, $ionicPopup, $timeou
                     break;
             }
         },
-        step: function(title, description, callbackTransitions) {
+        step: function(title, description, transition) {
+            var callback = function(option) {
+                return function() {
+                    if (typeof(option) === 'function') {
+                        option();
+                    } else {
+                        $state.transitionTo(option.state, option.params);
+                    }
+                };
+            };
             popup = $ionicPopup.confirm({
                 title: title + this.closeHTML,
                 template: description,
                 buttons: [{
                     text: 'Volver',
-                    onTap: callbackTransitions.back
+                    onTap: callback(transition.back)
                 }, {
                     text: 'Seguir',
                     type: 'button-positive',
-                    onTap: callbackTransitions.next
+                    onTap: callback(transition.next)
                 }]
             });
         }
